0e1b62ba7b785b71f0c9710273270b684c1fe164
Nathan Borup <nathan.borup@gmail.com>
2017-02-08 21:22:13 -0700

    Updated class group to use hashset for uniqueness; Added GUI functionality

--- src/studentChat/ChatServer.java
+++ src/studentChat/ChatServer.java
@@ -1,42 +1,32 @@
 package studentChat;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
-/*
- Created by Nate on 1/27/2017.
- Main application for chat program
- Initializes students, assigns them to a group and give students their responses
- Prints student responses as a conversation
-*/
+/**
+ * Created by Nate on 1/27/2017.
+ * Initializes students, assigns them to a group and give students their responses
+ * Launches windows for chat selection
+ */
 public final class ChatServer {
 
     private final List<Group> groups;
 
-    // Constructor
     // Calls the createStudentList function to create students, and then creates groups with 2 students in them
     private ChatServer() {
-        List<Student> students = createStudentList();
-
+        HashSet<Student> students = createStudentList();
         groups = createGroups(students);
     }
 
-    // Main function
     // Initializes chatServer and starts the chat method
     public static void main(String[] args) {
         ChatServer chatServer = new ChatServer();
-
         chatServer.chat();
     }
 
     // Iterates through the groups assigning responses to students
     private void chat() {
         Dialogue dialogue = new Dialogue();
-        int counter = 1;
         for (Group group : groups) {
-            System.out.println("Group " + counter++);
-
             Student student1 = group.getStudent1();
             Student student2 = group.getStudent2();
             String[] messages = dialogue.getMessages();
@@ -45,30 +35,31 @@ public final class ChatServer {
                 addResponse(student2, messages[i]);
             }
         }
+        new GroupSelectorGUI(groups);
     }
 
     // Adds response to student and then prints out the student and message
     private void addResponse(Student student1, String message) {
         Response response = new Response(student1, message);
         student1.getResponses().add(response);
-        System.out.println(response);
     }
 
     // Iterates through students, two at a time and assigns them to a group, then sorts the group by student 1
-    private List<Group> createGroups(List<Student> students) {
+    private List<Group> createGroups(HashSet<Student> students) {
         List<Group> groups = new ArrayList<>();
-
-        for (int i = 0; i < students.size(); i++) {
-            Group group = new Group(students.get(i++), students.get(i));
+        int numGroups = students.size() / 2;
+        Iterator<Student> studentIterator = students.iterator();
+        for (int i = 0; i < numGroups; i++) {
+            Group group = new Group(studentIterator.next(),studentIterator.next());
             groups.add(group);
         }
         Collections.sort(groups);
         return groups;
     }
 
-    // Creates the students and then sorts them alphabetically by last name
-    private List<Student> createStudentList() {
-        List<Student> students = new ArrayList<>();
+    // Creates the students
+    private HashSet<Student> createStudentList() {
+        HashSet<Student> students = new HashSet<>();
         students.add(new Student("Ethan", "Brown"));
         students.add(new Student("Nathan", "Borup"));
         students.add(new Student("Michael", "Cullimore"));
@@ -79,7 +70,6 @@ public final class ChatServer {
         students.add(new Student("Lauren", "Ribeiro"));
         students.add(new Student("Tyler", "Toponce"));
         students.add(new Student("Trevor", "Marsh"));
-        Collections.sort(students);
         return students;
     }
 }
